2c5317cb089e9fdafdee3be564697b46b1faf2d6
GABRIEL HENRIQUE SILVA SOARES <22282119@CFP127ALUNO.LOCAL>
2023-03-27 11:05:54 -0300

    frist

---
+++ module/funcoes.js
@@ -0,0 +1,27 @@
+
+  var contatosJson = require('./contatos.js')
+
+  function getConta(contato) {
+  
+      let arrayConta = contato
+      let contatoArray = []
+      let contaJSON = false;
+  
+      contatosJson.contatos['whats-users'].forEach(function (numero) {
+  
+          if (numero.number == arrayConta) {
+              numero.contacts.forEach(function (contato) {
+                  contatoArray.push(contato)
+              })
+          }
+      })
+  
+      if (arrayConta.length > 0) {
+          contaJSON = {}
+          contaJSON.contatos = contatoArray
+      }
+      return contaJSON
+  }
+  module.exports = {
+      getConta
+  }
